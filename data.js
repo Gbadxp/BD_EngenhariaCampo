7c55824bde9ed05f35030a2d6740c4fcdf055126
Gbadxp <georgegomes33@gmail.com>
2026-03-28 09:21:48 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+const initialTeams = [
+    { id: 1, name: "Equipe 1" },
+    { id: 2, name: "Equipe 2" },
+    { id: 3, name: "Equipe 3" },
+    { id: 4, name: "Equipe 4" },
+    { id: 5, name: "Equipe 5" },
+    { id: 6, name: "Equipe 6" },
+    { id: 7, name: "Equipe 7" },
+    { id: 8, name: "Equipe 8" }
+];
